3046bf5ed18c4f2795a393310691a4fdbef57329
ron981010 <rcacarapi.98@gmail.com>
2023-06-23 23:57:34 -0400

    adding lesson 1

---
+++ server.js
@@ -0,0 +1,12 @@
+// express web  server
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Luc Orellana");
+});
+ 
+const port = 3000;
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});

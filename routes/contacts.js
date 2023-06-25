80ee0ac513d712f7c1e723a9adadd4292c1c5921
ron981010 <rcacarapi.98@gmail.com>
2023-06-24 23:45:12 -0400

    rest

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const { getDb, initDb } = require('../db/connect');
 
-const contactsController = require('../controllers/contacts');
+// Inicializar la base de datos antes de usarla
+initDb((err) => {
+  if (err) {
+    console.error('Error initializing database:', err);
+    // Manejar el error de inicialización de la base de datos
+  } else {
+    // Configurar las rutas y controladores que utilizan la base de datos
+    const contactsController = require('../controllers/contacts');
 
-router.get('/', contactsController.getAll);
+    router.get('/', contactsController.getAll);
 
-router.get('/:id', contactsController.getSingle);
+    router.get('/:id', contactsController.getSingle);
+
+    // ...
+  }
+});
 
 module.exports = router;
